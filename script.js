c11ef424db229c7c74120ca842e1a6d7d1086660
Yasmim Lazarim <yasslazarim@gmail.com>
2024-08-15 18:12:03 -0300

    Create the playPauseDecider function

--- script.js
+++ script.js
@@ -3,7 +3,29 @@ const song = document.querySelector('.audio')
 const play = document.querySelector('.button-player')
 
 songName.innerText = 'Mockingnbird';
+let isPlaying = false;
 
-play.addEventListener('click', () => {
-    song.play()
-})
+function playSong(){
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill')
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill')
+    song.play();
+    isPlaying = true;
+}
+
+function pauseSong(){
+    play.querySelector('.bi').classList.add('bi-play-circle-fill')
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill')
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider(){
+    if(isPlaying === true){
+        pauseSong()
+    } 
+    else{
+        playSong()
+    }
+}
+
+play.addEventListener('click', playPauseDecider);
